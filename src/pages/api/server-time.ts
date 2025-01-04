48793284d4c20d8d06b2d6122ba291e53921cb14
Payam Zahedi <pzahedi1995@gmail.com>
2025-01-04 19:25:01 +0100

    fix: remove server-time file content

---
+++ src/pages/api/server-time.ts
@@ -0,0 +1,12 @@
+// export async function GET() {
+//   return new Response(
+//     JSON.stringify({
+//       serverTime: new Date().toISOString(),
+//     }),
+//     {
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     }
+//   );
+// }
