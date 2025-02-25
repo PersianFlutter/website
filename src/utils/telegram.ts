9577a0a93d8127e32deeee1e6642de0903c43fac
shahriaarrr <shahriaarrr@gmail.com>
2025-02-25 11:47:58 +0330

    add telegram channel members in home page

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,2 +1,16 @@
 import dotenv from "dotenv";
 import axios from "axios";
+
+dotenv.config();
+
+const botToken = process.env.TELEGRAM_BOT_TOKEN
+
+export async function telegramChannelMembers(channelName: string): Promise<number> {
+    try {
+        const response = await axios.get(`https://api.telegram.org/bot${botToken}/getChatMembersCount?chat_id=@${channelName}`);
+        return response.data.result;
+      } catch (error) {
+        console.error('Error fetching Telegram members count:', error);
+        return 0;
+      }
+}
