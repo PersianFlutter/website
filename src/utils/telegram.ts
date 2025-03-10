dfbd096ce4f9ccae0dd1cedde538ec14b2abb118
Payam Zahedi <pzahedi1995@gmail.com>
2025-03-10 18:04:22 +0100

    fix: run npm run fix

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-import axios from "axios";
+import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -7,7 +7,9 @@ const botToken = process.env.TELEGRAM_BOT_TOKEN;
 
 export async function telegramChannelMembers(channelName: string): Promise<number> {
   try {
-    const response = await axios.get(`https://api.telegram.org/bot${botToken}/getChatMembersCount?chat_id=@${channelName}`);
+    const response = await axios.get(
+      `https://api.telegram.org/bot${botToken}/getChatMembersCount?chat_id=@${channelName}`
+    );
     return response.data.result;
   } catch (error) {
     console.error('Error fetching Telegram members count:', error);
